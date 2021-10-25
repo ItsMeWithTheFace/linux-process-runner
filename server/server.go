3504f891fa96baacd86f1ce329b2e96c3f269ada
rakinuddin <rakin@uddin.io>
2021-10-24 23:03:02 -0500

    adds most server code

---
+++ server/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/ItsMeWithTheFace/linux-process-runner/api"
+	pb "github.com/ItsMeWithTheFace/linux-process-runner/api/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 8080))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterJobRunnerServiceServer(grpcServer, api.InitializeJobRunnerServer())
+	grpcServer.Serve(lis)
+}
