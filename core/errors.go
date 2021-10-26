07a5d2d975dabc399971d480b9d7844f7dd442b6
rakinuddin <rakin@uddin.io>
2021-10-25 22:28:30 -0500

    adds illegal state error

--- core/errors.go
+++ core/errors.go
@@ -4,6 +4,12 @@ import "fmt"
 
 type ErrNotFound struct{}
 
+type ErrIllegalStateChange struct{}
+
 func (e *ErrNotFound) Error() string {
 	return fmt.Sprintf("asset not found")
+}
+
+func (e *ErrIllegalStateChange) Error() string {
+	return fmt.Sprintf("attempted to change from terminal state")
 }
